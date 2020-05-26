f414d37b2c199baff31b59016ac9c5bbf4274129
hanswet <65296643+hanswet@users.noreply.github.com>
2020-05-26 13:51:53 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#this set of functions saves time by caching the information, 
+# to be called upon at a later time. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  J <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
+#this function explains te formation of the cache matrix. This includes
+# inverse functions, where j is the null vector. 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  y <<- x
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("receiving cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
 }
+#this code forms the cachesolve with a filler funtion, which 
+#includes the character vector to form the illogical function, 
+#"if" function, and the "return" function. 
